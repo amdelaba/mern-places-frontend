f187e3ce9b772e7dc694df3c2fe1a2d718c17581
Andres De La Barra <adelabarra24@gmail.com>
2020-11-05 01:11:06 -0500

    Added PlaceList and PlaceItem. Created reusable button and Modal

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'Famous Skyscraper 1',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building 2',
+    description: 'Famous Skyscraper 2',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  },
+
+];
 
 const UserPlaces = () => {
-  return <h2>User Places Works!!</h2>
+
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return (
+    <PlaceList items={loadedPlaces}/>
+  )
 };
 
-export default UserPlaces;
+export default UserPlaces; 
