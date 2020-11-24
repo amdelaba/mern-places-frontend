b84d150373f203ff7b188be4289262380e934a52
Andres De La Barra <adelabarra24@gmail.com>
2020-11-23 23:35:31 -0500

    Removed creator field from create place request since it is now encoded in the JWT

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -48,7 +48,6 @@ const NewPlace = () => {
       formData.append('description', formState.inputs.description.value );
       formData.append('address', formState.inputs.address.value );
       formData.append('image', formState.inputs.image.value );
-      formData.append('creator', auth.userId );
       const responseData = await sendRequest(
         'http://localhost:5000/api/places',
         'POST', 
