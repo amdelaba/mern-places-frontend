64ab20693a825efcd4fb07fc2593d92c7279d9ef
Andres De La Barra <adelabarra24@gmail.com>
2020-11-23 23:12:45 -0500

    Refactored authentication logic into auth-hook custom hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Users from './user/pages/Users';
@@ -7,60 +7,14 @@ import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './places/pages/Auth';
-import { AuthContext } from './shared/context/auth.context'
-
-let logoutTimer;
+import { AuthContext } from './shared/context/auth.context';
+import { useAuth } from './shared/hooks/auth-hook';
 
 function App() {
-  const [token, setToken] = useState(null);
-  const [userId, setUserId] = useState(null);
-  const [tokenExpiration, setTokenExpiration] = useState();
-
-  const login = useCallback((uid, token, expirationDate) =>{
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate = 
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); // Now + 1h
-    setTokenExpiration(tokenExpirationDate);
-      localStorage.setItem('userData', JSON.stringify({
-      userId: uid,
-      token,
-      expiration: tokenExpirationDate.toISOString()
-    }));
-  }, []);
-
-  const logout = useCallback(() =>{
-    setToken(null);
-    setUserId(null);
-    setTokenExpiration(null);
-    localStorage.removeItem('userData');
-  }, []);
-
-  // Whenever our token changes (ie login, logout)
-  // Creates a timer to logout when JWT expires
-  useEffect(() => {
-    if (token && tokenExpiration) {
-      const remainingTime = tokenExpiration.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpiration]);
-
-  // Will run on mount
-  // Checks localstorage for JWT, and if so logs user
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if(
-      storedData && 
-      storedData.token && 
-      new Date(storedData.expiration) > new Date()  // if stored date is still later than now
-    ) {
-      login(storedData.userId, storedData.token, new Date(storedData.expiration));
-    }
-  }, [login]);
+  const { userId, token, login, logout } = useAuth();
 
   let routes;
+
   if (token) {
     routes = (
       <Switch>
@@ -108,7 +62,7 @@ function App() {
       <Router>
         <MainNavigation/>
         <main>
-            {routes}
+          {routes}
         </main>
       </Router>
     </AuthContext.Provider>
---
+++ src/shared/hooks/auth-hook.js
@@ -0,0 +1,55 @@
+import React, { useState, useCallback, useEffect } from 'react';
+
+let logoutTimer;
+
+export const useAuth = () => {
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [tokenExpiration, setTokenExpiration] = useState();
+
+  const login = useCallback((uid, token, expirationDate) =>{
+    setToken(token);
+    setUserId(uid);
+    const tokenExpirationDate = 
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); // Now + 1h
+    setTokenExpiration(tokenExpirationDate);
+      localStorage.setItem('userData', JSON.stringify({
+      userId: uid,
+      token,
+      expiration: tokenExpirationDate.toISOString()
+    }));
+  }, []);
+
+  const logout = useCallback(() =>{
+    setToken(null);
+    setUserId(null);
+    setTokenExpiration(null);
+    localStorage.removeItem('userData');
+  }, []);
+
+  // Whenever our token changes (ie login, logout)
+  // Creates a timer to logout when JWT expires
+  useEffect(() => {
+    if (token && tokenExpiration) {
+      const remainingTime = tokenExpiration.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpiration]);
+
+  // Will run on mount
+  // Checks localstorage for JWT, and if so logs user
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if(
+      storedData && 
+      storedData.token && 
+      new Date(storedData.expiration) > new Date()  // if stored date is still later than now
+    ) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
+    }
+  }, [login]);
+
+  return { userId, token, login, logout,  }
+};
